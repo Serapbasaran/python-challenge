e4fdbc43cb30ec5b73f17ce87931484878bfab94
Serapbasaran <serap.cbasaran@gmail.com>
2020-10-06 12:06:13 -0400

    Update Main.py

--- Pypoll/Main.py
+++ Pypoll/Main.py
@@ -10,25 +10,79 @@
 import os
 import csv
 
-# Change directory to the directory of current python script
+# Change directory to the directory of current python scrip
 os.chdir(os.path.dirname(__file__))
 
-
 # Path to collect data from the Resources folder
-election_data_csv_path = os.path.join("Resources", "book1.csv")
+election_data_csv = os.path.join("Resources", "election_data.csv")
 
-#Define variables
-total_votes=0
 
 # Open and read csv
-with open(election_data_csv_path, newline="") as csvfile:
+with open(election_data_csv, newline="") as csvfile:
 
     csv_reader = csv.reader(csvfile, delimiter=",")
  
     # Read the header row first
     csv_header = next(csvfile)
 
-    for row in csv_reader:  
-        total_votes = total_votes + 1
+    candidate = []
 
-print(f"Total votes: {total_votes}")
+    for row in csv_reader:
+        candidate.append(row[2])
+
+    candidate_count = [[x,candidate.count(x)] for x in set(candidate)]
+    
+    votes = []
+    name = []
+    
+    for row in candidate_count:
+        name.append(row[0])
+        votes.append(row[1])
+
+    candidate_zip = zip(name, votes)
+    candidate_list = list(candidate_zip)
+
+    winner = max(votes)
+
+    for row in candidate_list:
+        if row[1] == winner:
+            winner_name = row[0]       
+            
+total_votes = len(candidate)
+
+correy_total = candidate.count('Correy')
+correy_percent = int(correy_total) / int(total_votes)
+
+o_tooley_total = candidate.count("O'Tooley")
+o_tooley_percent = o_tooley_total / total_votes
+
+li_total = candidate.count('Li')
+li_percent = li_total / total_votes
+
+khan_total = candidate.count('Khan')
+khan_percent = khan_total / total_votes
+
+print(f'Election Results')
+print(f'-------------------------')
+print(f'Total Votes: {total_votes}')
+print(f'-------------------------')
+print(f'Khan: {khan_percent:.3%} ({khan_total})')
+print(f'Correy: {correy_percent:.3%} ({correy_total})')
+print(f'Li: {li_percent:.3%} ({li_total})')
+print(f"O'Tooley: {o_tooley_percent:.3%} ({o_tooley_total})")
+print(f'-------------------------')
+print(f'Winner: {winner_name}')
+print(f'-------------------------')
+
+with open('PyPoll.txt', 'w') as text_file:
+    print(f'Election Results', file=text_file)
+    print(f'-------------------------', file=text_file)
+    print(f'Total Votes: {total_votes}', file=text_file)
+    print(f'-------------------------', file=text_file)
+    print(f'Khan: {khan_percent:.3%} ({khan_total})', file=text_file)
+    print(f'Correy: {correy_percent:.3%} ({correy_total})', file=text_file)
+    print(f'Li: {li_percent:.3%} ({li_total})', file=text_file)
+    print(f"O'Tooley: {o_tooley_percent:.3%} ({o_tooley_total})", file=text_file)
+    print(f'-------------------------', file=text_file)
+    print(f'Winner: {winner_name}', file=text_file)
+    print(f'-------------------------', file=text_file)  
